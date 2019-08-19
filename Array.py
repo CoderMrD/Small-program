eb6803567da00995a36e733aa10552c9783d4365
CoderMrD <31618160+CoderMrD@users.noreply.github.com>
2019-08-19 15:03:03 +0800

    Update Array.py

--- Array.py
+++ Array.py
@@ -1,6 +1,8 @@
 1. 查找列表中的任意一个重复元素
 l = [2, 3, 1, 0, 2, 5, 3]
-def duplicate(l):
+def duplicate(l: list):
+    if not isinstance(l, list):
+        return "Please input list"
     for i in range(len(l)):
         while i != l[i]:
             temp = l[i]
@@ -8,4 +10,5 @@ def duplicate(l):
                 l[i], l[temp] = l[temp], l[i]
             else:
                 return temp
+    return None
 时间复杂度O(n),空间复杂度O(1)
